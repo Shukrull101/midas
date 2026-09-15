b6b33f1526a59397f25152515d43816f2d8851e6
Shukrullo Barotov <shukrullobarotov4@gmail.com>
2026-09-15 23:16:15 +0500

    fix: resolve casing for vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import Header from 'src/layout/header/header';
+import Header from './layout/header/header';
 
 function App() {
   return (
-    <div style={{ backgroundColor: '#050b1f', minHeight: '100vh', color: '#fff' }}>
+    <div
+      style={{
+        backgroundColor: '#050b1f',
+        minHeight: '100vh',
+        color: '#fff'
+      }}
+    >
       <Header />
     </div>
   );
 }
-
-
-
-
-
 
 export default App;
